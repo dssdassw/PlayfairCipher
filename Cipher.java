d24a3ecc56e53b1960becf4c33f0ab88a58bb626
dssdassw <dssdassw.orig@gmail.com>
2016-04-25 20:50:43 -0400

    string processing done

--- Cipher.java
+++ Cipher.java
@@ -1,146 +1,48 @@
-/*
-Encrypting:
-encrypt 2 letters at a time
-	P1 = 't', P2 = 'h'
-	let C1, C2 be the corresponding encoded letters (P1 -> C1, P2 -> C2)
-
-
-Refer to the cipher table you created:
-	if P1, P2 on same row: C1, C2 are the cells directly to the right
-	if P1, P2 on same column: C1, C2 are the cells directly below
-	if P1, P2 are opposite corners of some rectangle:
-	C1, C2 complete the rectangle with C1 and P1 on the same row
-
-Tests:
-	Encrypt “the secret is safe”
-	> thesecretissafe
-	if there are 2 of the same letter paired together, insert an x
-	> thesecretisxsafe
-
-	th->uf (rule 3: rectangle)
-	es->re (rule 2: same column)
-	ec->tz (rule 3: rectangle)
-	re->zr (rule 1: same row)
-	ti->uy (rule 3: rectangle)
-	sx->qz (rule 3: rectangle)
-	sa->hv (rule 3: rectangle)
-	fe->st (rule 3: rectangle)
-	encrypted phrase: ufretzzruyqzhvst
-
-	65-90 & 97-122
-	
-	
-*/
-
-
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Cipher {
-	private static int[][] setup() { //working with ints during operation, btu will cast to char before display
-		Random r = new Random();
-		int randnum = 0;
-		int counter = 0;
-		int cipher[][] = new int[5][5];
-		int[] used = new int[25];
-		for (int a = 0; a<5; a++) {
-			counter = counter + 1;
-			for (int b = 0; b<5; b++) {
-				System.out.println(counter);
-				boolean found = false;
-				randnum =  r.nextInt((26)) + 97; //26 is 122-96+1, maximumAsciiLowercaseAlphabetNumber-minimumAsciiLowercaseAlphabetNumber+1
-				for (int i = 0; i<25; i++) { //25 being the maximum index of the list
-					if (used[i] == randnum) {
-						found = true; //to signal that it was not a clean end
-						break;
-					}
-				}
-				if (found) {
-					System.out.println("Found " + randnum);
-					//add an extra iteration:
-					if(b>0) {
-						b = b - 1;	
-					}
-					else {
-						a = a - 1;
-						b = 3;
-					}
-				}
-				else { //will make better after
-					used[counter] = randnum;
-					cipher[a][b]  = randnum;
-				}
-				System.out.println("a = " + a);
-				System.out.println("b = " + b);
-			}
-		}
-		return cipher;
+	
+	private static int[][] setup() { //returns a multidimensional int array with the ASCII values of every cipher letter in order
+		return new int[][] {{122, 99, 98, 109, 120},    //z c b m x
+							{118, 110, 97, 103, 108},   //v n a g l
+							{115, 102, 104, 107, 113},  //s f h k q
+							{101, 116, 117, 111, 119},  //e t u o w
+							{114, 121, 105, 112, 100}}; //r y i p d
 	}
+	
 	public static void main(String[] args) {
 		int[][] playfair = setup();
 		System.out.println(playfair[4][4]);
 		Scanner in = new Scanner(System.in);
 		System.out.println("Input the statement you would like to encode: ");
 		String rawstr = in.nextLine();
-		rawstr.replaceAll(" ", "");
-		rawstr.replaceAll("[a-z][a-z]", "x"); //x is ascii code 120, all doubles of a character are replaced with it
-		int[] unenc = new int[statement.length-1]; //assuming it's zero-indexed
-		System.out.println(statement);
-		for (int i = 0; i < unenc.length; i++) {
-			unenc[i] = (int)statement[i]; //unenc retrieves the ascii decimal representation of each character in the char[] array
+		in.close();
+		rawstr = rawstr.replaceAll(" +", "");
+		char[] raw = rawstr.toCharArray();
+		int counter = 0;
+		for (int c = 1; c<raw.length; c++) { //starts at 1 to avoid out of bounds error
+			if(raw[c] == raw[c-1]) {         //if it finds the same character beside itself:
+				char[] postMatchStorage = new char[raw.length - c]; //creates an array to store everything (including the second instance of the character) after the match
+				for (int charsAfterMatch = c; charsAfterMatch<raw.length; charsAfterMatch++) { //stores them in that array
+					postMatchStorage[counter] = raw[c+counter];
+					counter = counter + 1;
+				}
+				counter = 0;
+				raw = Arrays.copyOf(raw, raw.length + 1); //adds one to the array's length, thanks to StackOverflow user Evgeniy Dorofeev for their Java knawledge
+				raw[c] = 'x'; //inserts x in-between the duplicated characters
+				for (int elementToFill = c + 1; elementToFill<raw.length; elementToFill++) { //fills in the rest of the array with everything that was after the match
+					raw[elementToFill] = postMatchStorage[counter];
+					counter = counter + 1;
+				} //done!
+				counter = 0;
+			}
+		}
+		System.out.println(new String(raw));
+		int[] intcode = new int[raw.length-1]; //begins converting every character to an int before processing, the final step
+		for (int i = 0; i < intcode.length; i++) { //gets ready to iterate
+			intcode[i] = (int)raw[i]; //intcode retrieves the ascii decimal representation of each character in the char[] array
 		}
 	}
 }
-
-/*
-How to set a whole row
-
-//suppose you wanted to set row 2...
-int M[][] = new int[5][5]
-for (int column=0; column<5; column++) {
-	M[2][column]=1;
-}
-
-
-How to set a whole column				
-
-//suppose you wanted to set column 3...
-int M[][] = new int[5][5]
-for (int row=0; row<5; row++) {
-	M[row][3]=0;
-}
-
-
-Multiply all odd numbers in the grid by 2
-
-for (int r=0; r<5; r++) {
-	for (int c=0; c<5; c++) {
-		if (M[r][c] % 2 == 1) {
-M[r][c] = M[r][c] * 2;
-}
-	}
-}
-
-
-Add 1 to all even locations in the grid		
-for (int r=0; r<5; r++) {
-	for (int c=0; c<5; c++) {
-		if (M[r][c] % 2 == 0) {
-M[r][c] = M[r][c] + 1;
-}
-	}
-}
-
-
-
-Search for the largest value in a row
-
-int row = 2; //use row 2
-int largest = M[row][0]; //set largest to column 0 of row 2 as a start point
-for (int col=1; col<5; col++) {
-if (M[row][col]>largest) {
-	largest = M[row][col];
-}		
-}
-//after the loop is done the variable largest contains the largest value in the row
-*/
