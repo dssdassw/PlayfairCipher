77fb0e9cfdbc7cd93cbf94e4eccff22c57ae2f52
school <Eric.Sartor.024@students.hcdsb.org>
2016-04-28 13:09:46 -0400

    Fucked up decrypt but all special cases handled

--- Cipher.java
+++ Cipher.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 
 //SuppressWarnings("serial")
 public class Cipher extends JFrame {
+	private static final long serialVersionUID = 1L;
 	private JPanel root;
 	private JTextField inputField;
 	private JLabel resultLabel, instructions;
@@ -88,7 +89,7 @@ public class Cipher extends JFrame {
 						counter = counter + 1;
 					}
 					counter = 0;
-					raw = Arrays.copyOf(raw, raw.length + 1); //adds one to the array's length, thanks to StackOverflow user Evgeniy Dorofeev for their Java knawledge
+					raw = Arrays.copyOf(raw, raw.length + 1); //adds 1 to the array length
 					raw[c] = 'x'; //inserts x in-between the duplicated characters
 					for(int elementToFill = c + 1; elementToFill<raw.length; elementToFill++) { //fills in the rest of the array with everything that was after the match
 						raw[elementToFill] = postMatchStorage[counter];
@@ -97,27 +98,44 @@ public class Cipher extends JFrame {
 					counter = 0;
 				}
 			}
+			if((raw.length % 2) != 0) { //adjusts array length by adding an x to the end if it does not have an even number of letters
+				System.out.println("Array length is not even, adding an 'x'");
+				raw = Arrays.copyOf(raw, raw.length + 1); //adds one to the array's length, thanks to StackOverflow user Evgeniy Dorofeev for their Java knawledge
+				raw[raw.length-1] = 'x';
+			}
 			//System.out.println(new String(raw));
 			String result = new String("");
+			counter = 0; //still using counter
+			int[] jcases = new int[raw.length];
+			Arrays.fill(jcases, -1); //fills jcases with -1s so that the first character is not always assumed to be a j upon display (default = filled with 0)
 			int[] intcode = new int[raw.length]; //begins converting every character to an int before processing, the final step
-			for (int i = 0; i < intcode.length; i++) { //gets ready to iterate
-				intcode[i] = (int)raw[i]; //intcode retrieves the ascii decimal representation of each character in the char[] array
+			for(int p = 0; p < intcode.length; p++) { //gets ready to iterate
+				if(raw[p]=='j') { //handles the special case of j by replacing it with a z
+					System.out.println("Found a 'j' at " + p);
+					jcases[counter] = p;
+					raw[p] = 'z';
+					counter += 1;
+				}
+				intcode[p] = (int)raw[p]; //intcode retrieves the ascii decimal representation of each character in the char[] array
 			} //string processing is now complete!
 			if(!mode) { //encrypt
 				for(int i = 0; i<intcode.length; i+=2) { //searches for the letters in the array
 					//System.out.print((char)intcode[i]);
 					//System.out.println((char)intcode[i+1]);
 					int indexAx = -1, indexAy = -1, indexBx = -1, indexBy = -1; //set to -1 to check for not-found
-					for (int y = 0; y < playfair.length; y++) {
-						for (int x = 0; x < playfair[y].length; x++) {
-							if (playfair[y][x] == intcode[i]) {
+					for(int y = 0; y < playfair.length; y++) {
+						for(int x = 0; x < playfair[y].length; x++) {
+							if(playfair[y][x] == intcode[i]) {
 								indexAx = x;
 								indexAy = y;
 							}
 						}
 					}
-					if(indexAx == -1 | indexAy == -1) {
-						System.out.println("Values for some letters were not found!"); //deal with this error better later
+					if(indexAx == -1 | indexAy == -1) { //if a character is not found, it replaces it with x
+						System.out.println("Values for a letter was not found!");
+						System.out.println("Replacing this value with the letter 'x'");
+						indexAx = 4;
+						indexAy = 0;
 					}
 					for(int y = 0; y < playfair.length; y++) {
 						for (int x = 0; x < playfair[y].length; x++) {
@@ -128,11 +146,15 @@ public class Cipher extends JFrame {
 						}
 					}
 					if(indexBx == -1 | indexBy == -1) {
-						System.out.println("Values for some letters were not found!"); //same as above
+						System.out.println("Values for a letter was not found!");
+						System.out.println("Replacing this value with the letter 'x'");
+						indexBx = 4;
+						indexBy = 0;
 					}
 					//System.out.println((char)intcode[i] + ": (" + indexAx + ", " + indexAy + ") " + (char)intcode[i+1] + ": (" + indexBx + ", " + indexBy + ")");
 					//CASES:
 					if(indexAy == indexBy) { //SAME ROW
+						System.out.println("Same row");
 						if(indexAx+1>4) { //if it is the rightmost:
 							result = result + (char)playfair[indexAy][0]; //take the character on the leftmost
 						}
@@ -147,6 +169,7 @@ public class Cipher extends JFrame {
 						}
 					}
 					else if(indexAx == indexBx) { //SAME COLUMN
+						System.out.println("Same column");
 						if(indexAy+1>4) { //if it is in the bottom row with nothing below it...
 							result = result + (char)playfair[0][indexAx]; //take the character at the top
 						}
@@ -161,18 +184,19 @@ public class Cipher extends JFrame {
 						}
 					}
 					else { //OPPOSITE CORNERS MATCH
+						System.out.println("Rectangle");
 						result = result + (char)playfair[indexAy][indexBx];
 						result = result + (char)playfair[indexBy][indexAx];
 					}
 				}
-				resultLabel.setText(result);
+
 			}
 			else { //decrypt
 				for(int i = 0; i<intcode.length; i+=2) { //searches for the letters in the array
 					//System.out.print((char)intcode[i]);
 					//System.out.println((char)intcode[i+1]);
 					int indexAx = -1, indexAy = -1, indexBx = -1, indexBy = -1; //set to -1 to check for not-found
-					for (int y = 0; y < playfair.length; y++) {
+					for(int y = 0; y < playfair.length; y++) {
 						for (int x = 0; x < playfair[y].length; x++) {
 							if (playfair[y][x] == intcode[i]) {
 								indexAx = x;
@@ -180,8 +204,11 @@ public class Cipher extends JFrame {
 							}
 						}
 					}
-					if(indexAx == -1 | indexAy == -1) {
-						System.out.println("Values for some letters were not found!"); //deal with this error better later
+					if(indexAx == -1 | indexAy == -1) { 
+						System.out.println("Values for a letter was not found!");
+						System.out.println("Replacing this value with the letter 'x'");
+						indexAx = 4;
+						indexAy = 0;
 					}
 					for(int y = 0; y < playfair.length; y++) {
 						for (int x = 0; x < playfair[y].length; x++) {
@@ -192,11 +219,15 @@ public class Cipher extends JFrame {
 						}
 					}
 					if(indexBx == -1 | indexBy == -1) {
-						System.out.println("Values for some letters were not found!"); //same as above
+						System.out.println("Values for a letter was not found!");
+						System.out.println("Replacing this value with the letter 'x'");
+						indexBx = 4;
+						indexBy = 0;
 					}
 					//System.out.println((char)intcode[i] + ": (" + indexAx + ", " + indexAy + ") " + (char)intcode[i+1] + ": (" + indexBx + ", " + indexBy + ")");
 					//CASES:
 					if(indexAy == indexBy) { //SAME ROW
+						System.out.println("Same row");
 						if(indexAx-1<0) { //if it is the leftmost:
 							result = result + (char)playfair[indexAy][4]; //take the character on the rightmost
 						}
@@ -211,6 +242,7 @@ public class Cipher extends JFrame {
 						}
 					}
 					else if(indexAx == indexBx) { //SAME COLUMN
+						System.out.println("Same column");
 						if(indexAy-1<0) { //if it is in the top row with nothing above it...
 							result = result + (char)playfair[4][indexAx]; //take the character at the bottom
 						}
@@ -225,12 +257,27 @@ public class Cipher extends JFrame {
 						}
 					}
 					else { //OPPOSITE CORNERS MATCH
+						System.out.println("Rectangle");
 						result = result + (char)playfair[indexBy][indexAx];
 						result = result + (char)playfair[indexAy][indexBx];
-					} //ufretzzruyqzhvst 
+					} //ufretzzruyqzhvst
+					System.out.println(result);
 				}
-				resultLabel.setText(result);
 			}
+			System.out.println(result);
+			char[] xcrypted = new char[result.length()];
+			xcrypted = result.toCharArray();
+			for(int j = 0; j < jcases.length; j++) { //because fuck it why not use j as the iteration var
+				if(jcases[j] != -1) { //the array is filled with -1s, because a value that represents an actual index of a j would never be at -1
+					System.out.println(jcases[j]);
+					xcrypted[jcases[j]] = 'j';
+				}
+			}
+			String xcryptedDisplay = new String();
+			for(int chr = 0; chr < xcrypted.length; chr++) {
+				xcryptedDisplay += xcrypted[chr];
+			}
+			resultLabel.setText(xcryptedDisplay);
 		}
 	}
 	
